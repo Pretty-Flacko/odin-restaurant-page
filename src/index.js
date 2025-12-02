6272b34aa619115a2c2f9e60a7c09c0f5d1029b1
Pretty-Flacko <48690875+Pretty-Flacko@users.noreply.github.com>
2025-12-03 02:58:59 +0400

    Add buttons functionality

--- src/index.js
+++ src/index.js
@@ -2,5 +2,11 @@ import "./style.css";
 import loadHome from "./home.js";
 import loadMenu from "./menu.js";
 
-// loadHome();
-loadMenu();
+const homeBtn = document.getElementById("btn-home");
+const menuBtn = document.getElementById("btn-menu");
+// const aboutBtn = document.getElementById("btn-about");
+
+homeBtn.addEventListener("click", loadHome);
+menuBtn.addEventListener("click", loadMenu);
+
+loadHome();
